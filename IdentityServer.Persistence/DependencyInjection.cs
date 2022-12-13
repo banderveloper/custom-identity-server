648efed937e346634b2e277980fb84cb6a818734
kpssnik <kpssdeveloper@gmail.com>
2022-12-13 22:41:03 +0200

    persistence new / Created persistence DI class

---
+++ IdentityServer.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Inject database context
+        var connectionString = configuration.GetConnectionString("Sqlite");
+        services.AddDbContext<IdentityDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        // Inject early created db context with interface 
+        services.AddScoped<IIdentityDbContext>(provider => 
+            provider.GetService<IdentityDbContext>());
+
+        return services;
+    }
+}
