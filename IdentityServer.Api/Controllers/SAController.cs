3c8ac45a4c4632d8cbf2cb72707824af2f478348
kpssnik <kpssdeveloper@gmail.com>
2022-12-14 01:13:28 +0200

    api new / Filled program.cs file, injected needed layers to service collection, initialized db, created system administrator controller (temporary without protection). Successfully created new role

---
+++ IdentityServer.Api/Controllers/SAController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using IdentityServer.Application.Requests.SA.Commands.CreateRole;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SAController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SAController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // [HttpPost("createRole")]
+    // public async Task<IActionResult> CreateRole([FromBody] CreateRoleModel model)
+    // {
+    //     await _mediator.Send(new CreateRoleCommand() { Name = model.Name });
+    //     return Ok();
+    // }
+}
--- IdentityServer.Api/Program.cs
+++ IdentityServer.Api/Program.cs
@@ -1,4 +1,31 @@
+using IdentityServer.Application;
+using IdentityServer.Application.Interfaces;
+using IdentityServer.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Inject other layer DI
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddControllers();
+
+// Initialize db
+try
+{
+    var scope = builder.Services.BuildServiceProvider().CreateScope();
+    var context = scope.ServiceProvider.GetService<IdentityDbContext>();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    // temporary
+    Console.WriteLine(ex);
+}
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
