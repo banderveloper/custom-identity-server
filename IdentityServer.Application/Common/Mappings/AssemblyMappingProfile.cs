d057b8f001ca6a14f619b73f9ccf883031456a7e
kpssnik <kpssdeveloper@gmail.com>
2022-12-13 15:59:33 +0200

    application new / Created interface for database context, and mapping core - interface ImapWith and AssemblyMappingProfile. Each model and DTO than needed to be mapped must inherit interface IMapWith and implement Mapping method, or do not if default mapping is ok

---
+++ IdentityServer.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace IdentityServer.Application.Common.Mappings;
+
+public class AssemblyMappingProfile : Profile
+{
+    public AssemblyMappingProfile(Assembly assembly)
+        => ApplyMappingsFromAssembly(assembly);
+
+    // Method invoked at MAIN, when server starts, applies mapping profiles (calls Mapping method) for each type that inherits from IMapWith
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        // Get all classes than implements IMapWith interface
+        var iMapWithImplementers = assembly.GetExportedTypes()
+            .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                          i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+            .ToList();
+
+        // run above each class that implements IMapWith interface
+        foreach (var iMapWithImplementer in iMapWithImplementers)
+        {
+            // locally creates instance of class than inherits from IMapWith
+            var instance = Activator.CreateInstance(iMapWithImplementer);
+
+            // gets CUSTOM Mapping method realisation, or DEFAULT realisation described in IMapWith
+            var method = iMapWithImplementer.GetMethod("Mapping");
+
+            // invokes method Mapping at given instance at pass AssemblyMappingProfile there
+            method?.Invoke(instance, new object?[] { this });
+        }
+    }
+}
---
+++ IdentityServer.Application/Interfaces/IIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer.Domain.IdentityUser;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Application.Interfaces;
+
+// Interface of main database context
+public interface IIdentityDbContext
+{
+    DbSet<IdentityUser> Users { get; set; }
+    DbSet<IdentityUserRole> Roles { get; set; }
+    DbSet<IdentityUserPersonal> Personals { get; set; }
+    
+    Task<int> SaveChangesAsync();
+}
